8dfc68dfd5e87c2d325c0e57c98a07383d1d9f5b
Sepehr Sadighpour <sepehr125@gmail.com>
2016-03-12 23:43:10 -0800

    added k-th top frequencies. No change in performance

--- create_features.py
+++ create_features.py
@@ -7,9 +7,10 @@ import os
 import pickle
 from collections import defaultdict
 import argparse
-from helpers import window_df, standardize, amplitudes
+from helpers import window_df, standardize, top_k_indices
 from settings import SAMPLING_RATE, CSV_FILES, VALID_TARGETS, \
                         DATA_COLS, TARGET_COL, COLS
+
 
 """
 This script:
@@ -56,6 +57,8 @@ if __name__ == '__main__':
     master_valid = master[master['target'].isin(VALID_TARGETS)]
 
     feature_matrix = []
+    # looping over groups helps make sure we don't
+    # consider windows with more than one target
     for target, df in master_valid.groupby('target'):
         
         # 0-center the mean and normalize. this adds about 5 points
@@ -63,69 +66,94 @@ if __name__ == '__main__':
         
         print("Processing %d rows for target #%d..."%(len(df), target))
 
-        new_group = defaultdict(list)
-        new_group['target'] = target
+        grp = defaultdict(list)
+        grp['target'] = target
         samples = window_df(df, args.n_seconds*SAMPLING_RATE, args.pct_overlap)
         for sample in samples:
             
+            # kurtoses = kurtosis(sample[DATA_COLS])
+            # grp['x_kurtosis'].append(kurtoses['x_accel'])
+            # grp['y_kurtosis'].append(kurtoses['y_accel'])
+            # grp['z_kurtosis'].append(kurtoses['z_accel'])
+
             means = sample[DATA_COLS].mean()
-            new_group['x_accel_mean'].append(means['x_accel'])
-            new_group['y_accel_mean'].append(means['y_accel'])
-            new_group['z_accel_mean'].append(means['z_accel'])
+            grp['x_accel_mean'].append(means['x_accel'])
+            grp['y_accel_mean'].append(means['y_accel'])
+            grp['z_accel_mean'].append(means['z_accel'])
             
             stds = sample[DATA_COLS].std()
-            new_group['x_accel_std'].append(stds['x_accel'])
-            new_group['y_accel_std'].append(stds['y_accel'])
-            new_group['z_accel_std'].append(stds['z_accel'])
+            grp['x_accel_std'].append(stds['x_accel'])
+            grp['y_accel_std'].append(stds['y_accel'])
+            grp['z_accel_std'].append(stds['z_accel'])
             
-            new_group['x_max_min'].append(max(sample.x_accel) - min(sample.x_accel))
-            new_group['y_max_min'].append(max(sample.y_accel) - min(sample.y_accel))
-            new_group['z_max_min'].append(max(sample.z_accel) - min(sample.z_accel))
+            grp['x_max_min'].append(max(sample["x_accel"]) - min(sample["x_accel"]))
+            grp['y_max_min'].append(max(sample["y_accel"]) - min(sample["y_accel"]))
+            grp['z_max_min'].append(max(sample["z_accel"]) - min(sample["z_accel"]))
             
             # correlations
             corrs = sample[DATA_COLS].corr()
-            new_group['xy_corr'].append(corrs.loc['x_accel', 'y_accel'])
-            new_group['xz_corr'].append(corrs.loc['x_accel', 'z_accel'])
-            new_group['yz_corr'].append(corrs.loc['y_accel', 'z_accel'])
+            grp['xy_corr'].append(corrs.loc['x_accel', 'y_accel'])
+            grp['xz_corr'].append(corrs.loc['x_accel', 'z_accel'])
+            grp['yz_corr'].append(corrs.loc['y_accel', 'z_accel'])
             
             # root-mean-square(x, y, z)
             rms = np.sqrt(np.mean(np.square(sample[DATA_COLS]), axis=1))
-            new_group['rms_mean'].append(rms.mean())
-            new_group['rms_std'].append(rms.std())
+            grp['rms_mean'].append(rms.mean())
+            grp['rms_std'].append(rms.std())
 
-            
-            # fourier transforms! 
-            x_fft = amplitudes(sample['x_accel'])
-            y_fft = amplitudes(sample['y_accel'])
-            z_fft = amplitudes(sample['z_accel'])
-           
+            # fourier transforms!
+            # x_fft = abs(np.fft.rfft(sample['x_accel']))
+            # y_fft = abs(np.fft.rfft(sample['y_accel']))
+            # z_fft = abs(np.fft.rfft(sample['z_accel']))
+            freqs = np.fft.rfftfreq(len(sample), 1./SAMPLING_RATE) # x-axis vals in Hz
+
+            # filter out frequencies
+            min_freq = 1. # very low frequencies tend to blow up amplitudes
+            # take top frequencies
+            k = 5
+            for dim in DATA_COLS:
+                fourier = abs(np.fft.rfft(sample[dim]))
+                # ignore nonesense frequencies
+                valid_fouriers = fourier[freqs > min_freq]
+                valid_freqs = freqs[freqs > min_freq]
+
+                peaks = top_k_indices(valid_fouriers, k)
+                for kth, peak in enumerate(peaks):
+                    fft_col = "%s_fft_top_%d"%(dim,kth)
+                    grp[fft_col].append(valid_freqs[peak])
+
+                mean_col = "%s_fft_mean"%dim
+                grp[mean_col].append(fourier.mean())
+
+                std_col = "%s_fft_std"%dim
+                grp[std_col].append(fourier.std())
+
             # Max Fourier 
-            new_group['x_fft_max'].append(x_fft.max())
-            new_group['y_fft_max'].append(y_fft.max())
-            new_group['z_fft_max'].append(z_fft.max())
+            # grp['x_fft_max'].append(x_fft.max())
+            # grp['y_fft_max'].append(y_fft.max())
+            # grp['z_fft_max'].append(z_fft.max())
 
-            # Min Fourier
-            new_group['x_fft_min'].append(x_fft.min())
-            new_group['y_fft_min'].append(y_fft.min())
-            new_group['z_fft_min'].append(z_fft.min())
+            # # Min Fourier
+            # grp['x_fft_min'].append(x_fft.min())
+            # grp['y_fft_min'].append(y_fft.min())
+            # grp['z_fft_min'].append(z_fft.min())
             
             # Mean Fourier
-            new_group['x_fft_mean'].append(x_fft.mean())
-            new_group['y_fft_mean'].append(y_fft.mean())
-            new_group['z_fft_mean'].append(z_fft.mean())
+            # grp['x_fft_mean'].append(x_fft.mean())
+            # grp['y_fft_mean'].append(y_fft.mean())
+            # grp['z_fft_mean'].append(z_fft.mean())
 
-            # Standard deviation Fourier
-            new_group['x_fft_std'].append(x_fft.std())
-            new_group['y_fft_std'].append(y_fft.std())
-            new_group['z_fft_std'].append(z_fft.std())
+            # # Standard deviation Fourier
+            # grp['x_fft_std'].append(x_fft.std())
+            # grp['y_fft_std'].append(y_fft.std())
+            # grp['z_fft_std'].append(z_fft.std())
 
-
-        # Add new_group to feature_matrix
-        feature_matrix.append(pd.DataFrame(new_group))
+        # Add grp to feature_matrix
+        feature_matrix.append(pd.DataFrame(grp))
 
     # concatenate all groups into one dataframe
     feature_matrix_df = pd.concat(feature_matrix)
 
-
+    # save features
     with open(args.output_file, 'wb') as f:
         pickle.dump(feature_matrix_df, f)
